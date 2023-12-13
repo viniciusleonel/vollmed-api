86cbabcf2242e505843dd2f90ab29e5703d7126e
Vinicius Leonel <viniciuslps.cms@gmail.com>
2023-12-13 17:17:56 -0300

    Adicionando exclusão lógica

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -25,7 +25,11 @@ public class Medico {
     @Embedded
     private Endereco endereco;
 
+
+    private Boolean ativo;
+
     public Medico (DadosCadastroMedico dados){
+        this.ativo = true;
         this.nome = dados.nome();
         this.email = dados.email();
         this.telefone = dados.telefone();
@@ -46,5 +50,9 @@ public class Medico {
         }
 
 
+    }
+
+    public void excluir() {
+        this.ativo = false;
     }
 }
