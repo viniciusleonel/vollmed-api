f4082e8843cfd5214d28f088337c1a811fc5d7bc
Vinicius Leonel <viniciuslps.cms@gmail.com>
2024-01-06 16:45:36 -0300

    iniciando implementação do agendamento de consultas

---
+++ src/main/java/med/voll/api/domain/consulta/Consulta.java
@@ -0,0 +1,31 @@
+package med.voll.api.domain.consulta;
+
+import lombok.*;
+import jakarta.persistence.*;
+import med.voll.api.domain.medico.Medico;
+import med.voll.api.domain.paciente.Paciente;
+
+import java.time.LocalDateTime;
+
+@Table(name = "consultas")
+@Entity(name = "Consulta")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Consulta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "medico_id")
+    private Medico medico;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "paciente_id")
+    private Paciente paciente;
+
+    private LocalDateTime data;
+}
