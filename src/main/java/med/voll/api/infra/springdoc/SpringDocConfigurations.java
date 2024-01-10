0b2d212f1521e84261a68d8d36d7d0b80c63999e
Vinicius Leonel <viniciuslps.cms@gmail.com>
2024-01-10 18:04:19 -0300

    adicionando mais informações no SpringDoc

--- src/main/java/med/voll/api/infra/springdoc/SpringDocConfigurations.java
+++ src/main/java/med/voll/api/infra/springdoc/SpringDocConfigurations.java
@@ -5,6 +5,9 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
 @Configuration
 public class SpringDocConfigurations {
@@ -14,8 +17,18 @@ public class SpringDocConfigurations {
         return new OpenAPI()
                 .components(new Components()
                         .addSecuritySchemes("bearer-key",
-                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                                .info(new Info()
+                                        .title("Voll.med API")
+                                        .description("API Rest da aplicação Voll.med, contendo as funcionalidades de CRUD de médicos e de pacientes, além de agendamento e cancelamento de consultas")
+                                        .contact(new Contact()
+                                                .name("Time Backend")
+                                                .email("viniciuslps.cms@gmail.com"))
+                                        .license(new License()
+                                                .name("Apache 2.0")
+                                                .url("http://voll.med/api/licenca")));
     }
-
-
 }
