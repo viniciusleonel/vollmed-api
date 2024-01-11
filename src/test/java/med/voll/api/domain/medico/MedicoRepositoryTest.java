a26e170944fd62006e14bce0230961200a20cc58
Vinicius Leonel <viniciuslps.cms@gmail.com>
2024-01-11 18:36:02 -0300

    Automatizando testes com JUnit e Spring

---
+++ src/test/java/med/voll/api/domain/medico/MedicoRepositoryTest.java
@@ -0,0 +1,223 @@
+package med.voll.api.domain.medico;
+
+import med.voll.api.domain.consulta.Consulta;
+import med.voll.api.domain.consulta.ConsultaRepository;
+import med.voll.api.domain.endereco.DadosEndereco;
+import med.voll.api.domain.paciente.DadosCadastroPaciente;
+import med.voll.api.domain.paciente.Paciente;
+import med.voll.api.domain.paciente.PacienteRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.time.DayOfWeek;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.TemporalAdjusters;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+class MedicoRepositoryTest {
+
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
+    @Autowired
+    private ConsultaRepository consultaRepository;
+
+    @Autowired
+    private TestEntityManager em;
+
+    @Test
+    @DisplayName("Deveria devolver null quando único medico cadastrado não está disponível na data")
+    void escolherMedicoAleatorioLivreNaDataCenario1() {
+        // given ou arrange
+        var proximaSegundaAs10 = LocalDate.now()
+                .with(TemporalAdjusters.next(DayOfWeek.MONDAY))
+                .atTime(10,0);
+        var medico = cadastrarMedico("Medico", "medico@voll.med", "123456", Especialidade.Cardiologia);
+        var paciente = cadastrarPaciente("Paciente", "paciente@gmail.com", "12345678910");
+        cadastrarConsulta(medico, paciente, proximaSegundaAs10);
+
+        // when ou act
+        var medicoLivre = medicoRepository.escolherMedicoAleatorioLivreNaData(Especialidade.Cardiologia, proximaSegundaAs10);
+
+        // then ou assert
+        assertThat(medicoLivre).isNull();
+    }
+
+    @Test
+    @DisplayName("Deveria devolver médico quando ele estiver disponível na data")
+    void escolherMedicoAleatorioLivreNaDataCenario2() {
+        // given ou arrange
+        var proximaSegundaAs10 = LocalDate.now()
+                .with(TemporalAdjusters.next(DayOfWeek.MONDAY))
+                .atTime(10,0);
+        var medico = cadastrarMedico("Medico", "medico@voll.med", "123456", Especialidade.Cardiologia);
+
+        // when ou act
+        var medicoLivre = medicoRepository.escolherMedicoAleatorioLivreNaData(Especialidade.Cardiologia, proximaSegundaAs10);
+
+        // then ou assert
+        assertThat(medicoLivre).isEqualTo(medico);
+    }
+
+    @Test
+    @DisplayName("Verifica se médico está desativado")
+    void medicoDesativado() {
+        var medico = cadastrarMedico("Medico", "medico@voll.med", "123456", Especialidade.Cardiologia);
+
+        medico.excluir();
+
+        assertThat(medicoRepository.findAtivoById(medico.getId())).isFalse();
+    }
+
+    @Test
+    @DisplayName("Verifica se médico está ativo")
+    void medicoAtivo() {
+        var medico = cadastrarMedico("Medico", "medico@voll.med", "123456", Especialidade.Cardiologia);
+
+        assertThat(medicoRepository.findAtivoById(medico.getId())).isTrue();
+    }
+
+    @Test
+    @DisplayName("Consulta não pode ser agendada com menos de 30 minutos de antecedência ")
+    void horarioAntecedencia() {
+        var horaComMaisDe30MinDeAntecedencia = LocalDateTime.now();
+        var horaComMenosDe30MinDeAntecedencia = LocalDate.now()
+                .with(TemporalAdjusters.next(DayOfWeek.MONDAY))
+                .atTime(9,40);
+        var proximaSegundaAs10 = LocalDate.now()
+                .with(TemporalAdjusters.next(DayOfWeek.MONDAY))
+                .atTime(10,0);
+
+        var diferencaEntreMinutos = Duration.between(horaComMaisDe30MinDeAntecedencia, proximaSegundaAs10).toMinutes();
+        var diferencaEntreMinutos2 = Duration.between(horaComMenosDe30MinDeAntecedencia, proximaSegundaAs10).toMinutes();
+
+        assertThat(diferencaEntreMinutos).isGreaterThan(30);
+        assertThat(diferencaEntreMinutos2).isLessThan(30);
+    }
+
+    @Test
+    @DisplayName("Não cadastrar consulta fora do horário de funcionamento")
+    void horarioFuncionamentoClinica() {
+        var proximaSegundaAs10 = LocalDate.now()
+                .with(TemporalAdjusters.next(DayOfWeek.MONDAY))
+                .atTime(10,0);
+        var medico = cadastrarMedico("Medico", "medico@voll.med", "123456", Especialidade.Cardiologia);
+        var paciente = cadastrarPaciente("Paciente", "paciente@gmail.com", "12345678910");
+
+        var consulta = cadastrarEObterConsulta(medico, paciente, proximaSegundaAs10);
+
+        assertThat(consulta.getData().getDayOfWeek()).as("A consulta não deve ser agendada em um sábado ou domingo").isNotIn(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY);
+        assertThat(consulta.getData().getHour()).as("A consulta não deve ser agendada antes das 7 horas").isGreaterThanOrEqualTo(7);
+        assertThat(consulta.getData().getHour()).as("A consulta não deve ser agendada após as 18 horas").isLessThanOrEqualTo(18);
+
+    }
+
+    @Test
+    @DisplayName("Paciente não pode ter duas consultas no mesmo dia")
+    void pacienteSemOutraConsultaNoDia() {
+        var proximaSegundaAs10 = LocalDate.now()
+                .with(TemporalAdjusters.next(DayOfWeek.MONDAY))
+                .atTime(10,0);
+        var medico = cadastrarMedico("Medico", "medico@voll.med", "123456", Especialidade.Cardiologia);
+        var paciente = cadastrarPaciente("Paciente", "paciente@gmail.com", "12345678910");
+        var consulta = cadastrarEObterConsulta(medico, paciente, proximaSegundaAs10);
+
+        var primeiroHorario = consulta.getData().withHour(7);
+        var ultimoHorario = consulta.getData().withHour(18);
+
+        var pacientePossuiOutraConsultaNoDia = consultaRepository.existsByPacienteIdAndDataBetween(
+                paciente.getId(),
+                primeiroHorario,
+                ultimoHorario);
+
+        assertThat(pacientePossuiOutraConsultaNoDia).isTrue();
+    }
+
+    @Test
+    @DisplayName("Verifica se paciente está desativado")
+    void pacienteDesativado() {
+        var paciente = cadastrarPaciente("Paciente", "paciente@gmail.com", "12345678910");
+
+        paciente.excluir();
+
+        assertThat(pacienteRepository.findAtivoById(paciente.getId())).isFalse();
+    }
+
+    @Test
+    @DisplayName("Verifica se paciente está ativo")
+    void pacienteAtivo() {
+        var paciente = cadastrarPaciente("Paciente", "paciente@gmail.com", "12345678910");
+
+        assertThat(pacienteRepository.findAtivoById(paciente.getId())).isTrue();
+    }
+
+    private Medico cadastrarMedico (String nome, String email, String crm, Especialidade especialidade) {
+        var medico = new Medico(dadosMedico(nome, email, crm, especialidade));
+        em.persist(medico);
+        return medico;
+    }
+
+    private void cadastrarConsulta(Medico medico, Paciente paciente, LocalDateTime data) {
+        em.persist(new Consulta(null, medico, paciente, data, null));
+    }
+
+    private Consulta cadastrarEObterConsulta(Medico medico, Paciente paciente, LocalDateTime data) {
+        var consulta = new Consulta(null, medico, paciente, data, null);
+        em.persist(consulta);
+        return consulta;
+    }
+
+    private Paciente cadastrarPaciente (String nome, String email, String cpf) {
+        var paciente = new Paciente(dadosPaciente(nome, email, cpf));
+        em.persist(paciente);
+        return paciente;
+    }
+
+    private DadosCadastroMedico dadosMedico (String nome, String email, String crm, Especialidade especialidade){
+        return new DadosCadastroMedico(
+                nome,
+                email,
+                "119933107760",
+                crm,
+                especialidade,
+                dadosEndereco()
+        );
+    }
+
+    private DadosCadastroPaciente dadosPaciente(String nome, String email, String cpf) {
+        return new DadosCadastroPaciente(
+                nome,
+                email,
+                "11993310776",
+                cpf,
+                dadosEndereco()
+        );
+    }
+
+    private DadosEndereco dadosEndereco() {
+        return new DadosEndereco(
+                "rua xpto",
+                "bairro",
+                "00000000",
+                "São Paulo",
+                "SP",
+                null,
+                null
+        );
+    }
+}
