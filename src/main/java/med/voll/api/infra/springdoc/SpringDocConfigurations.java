37a74eb61fc2be1158ffe4a65294ab1a861c57b7
vinicius-leonel <viniciuslps.cms@gmail.com>
2024-10-01 15:11:36 -0300

    Update SpringDoc

--- src/main/java/med/voll/api/infra/springdoc/SpringDocConfigurations.java
+++ src/main/java/med/voll/api/infra/springdoc/SpringDocConfigurations.java
@@ -25,9 +25,11 @@ public class SpringDocConfigurations {
                                 .addSecurityItem(new SecurityRequirement().addList("bearer-key"))
                                 .info(new Info()
                                         .title("Voll.med API")
-                                        .description("A VollMed API é uma aplicação desenvolvida em Java com Spring Framework, destinada à gestão de uma clínica médica. Com recursos de autenticação JWT, a API permite o cadastro de médicos, pacientes e consultas, fornecendo endpoints públicos para login e cadastro de usuários.\n" +
-                                                "\n" +
-                                                "Foi implementado um workflow de Integração Contínua (CI) utilizando GitHub Actions para testes e builds em pull requests, e um processo de Entrega Contínua (CD) que realiza o deploy automático da aplicação em produção. A aplicação é containerizada com Docker e implantada na Azure, garantindo atualizações rápidas e escalabilidade.\n")
+                                        .description("""
+                                                A VollMed API é uma aplicação desenvolvida em Java com Spring Framework, destinada à gestão de uma clínica médica. Com recursos de autenticação JWT, a API permite o cadastro de médicos, pacientes e consultas, fornecendo endpoints públicos para login e cadastro de usuários.\
+                                                
+                                                Foi implementado um workflow de Integração Contínua (CI) utilizando GitHub Actions para testes e builds em pull requests, e um processo de Entrega Contínua (CD) que realiza o deploy automático da aplicação em produção. A aplicação é containerizada com Docker e implantada na Azure, garantindo atualizações rápidas e escalabilidade.
+                                                """)
                                         .contact(new Contact()
                                                 .name("Vinicius Leonel")
                                                 .email("viniciuslps.cms@gmail.com")
